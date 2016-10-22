f56018a9e2bb9ab882a9f3e1927bb8de851aec6b
nBukaty <nickbukaty@gmail.com>
2016-10-21 23:29:17 -0500

    started the activeTower class

---
+++ Assets/scripts/activeTower.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.scripts
+{
+    class activeTower : MonoBehaviour
+    {
+        public float shotSpeed = 1; //fire rate of the tower
+        //TODO: add a projectile object that takes in the tower damage
+        public float baseDamage = 1; //the base damage a tower deals 
+        private enemy currentTarget;
+        private List<enemy> targetList = new List<enemy>(); //a list of enemies in the attack radius
+
+        /**
+         * initialize the tower 
+         * call functions to initialize projectile
+         **/
+        void placeTower()
+        {
+
+        }
+        /**
+         * set the target of a tower to the first element in the target list
+         * TODO: add ability to attack the first, last, strongest, or weakest enemy
+         **/
+        void setTarget()
+        {
+            if (targetList.Count!=0)
+            {
+                currentTarget = targetList[0];
+            }
+        }
+        void attack()
+        {
+            //attack currentTarget
+        }
+    }
+}
