cb7b1ee390bea9f35c5c91e637a8732a8beed30b
Alex Griffin <agbassist@gmail.com>
2016-10-23 19:06:11 -0500

    Added a script for rotation of the active tower. Doesn't work yet, but I think it's not the script's fault.

---
+++ Assets/ActiveTowerRotate.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActiveTowerRotate : MonoBehaviour {
+
+    private Rigidbody body;
+    private float speed = 10f;
+    private static string inputName = "Horizontal";
+    private float inputValue;
+
+    void Awake()
+    {
+        body = GetComponent<Rigidbody>();
+        inputValue = 0f;
+        body.isKinematic = false;
+    }
+    // Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        inputValue = Input.GetAxis(inputName);
+        
+	}
+
+    private void FixedUpdate()
+    {
+        Turn();
+    }
+
+    private void Turn()
+    {
+        if(inputValue != 0)
+        {
+            float turn = speed * Time.deltaTime;
+
+            Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f); //Turns the tower around the y-axis
+
+            body.MoveRotation(body.rotation * turnRotation);
+        }
+        
+    }
+}
