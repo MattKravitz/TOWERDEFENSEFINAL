dfba7ad52bb7b9bcca6634f42e3039cd20893f25
Thomas Doty <Thomas Doty>
2016-10-25 22:39:45 -0500

    Added a script that adds a tower on click

---
+++ Assets/scripts/towerPlacement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class towerPlacement : MonoBehaviour {
+
+    private Vector3 placement;
+    private Vector3 down;
+    private int j;
+    public Transform tower;
+
+    // Use this for initialization
+    void Start () {
+	
+        placement = this.transform.position;
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        
+
+
+	}
+
+    void OnMouseDown()
+    {
+        
+        for (int i = 0; i < 255; i++)
+        {
+            if (placement == proceduralGeneration.points[i].position)
+            {
+                 j = i;
+            
+            }
+        }
+
+        Instantiate(tower, proceduralGeneration.redpoints[j].position + Vector3.up, proceduralGeneration.redpoints[j].rotation);
+
+    }
+}
