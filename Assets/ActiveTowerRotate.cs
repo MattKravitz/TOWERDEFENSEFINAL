08b525aaa708be9bfc6ab38040ecb88a9714c980
Alex Griffin <agbassist@gmail.com>
2016-10-30 17:39:41 -0500

    Added very basic bullet class

--- Assets/ActiveTowerRotate.cs
+++ Assets/ActiveTowerRotate.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class ActiveTowerRotate : MonoBehaviour {
 
-    private Rigidbody body;
+    public Transform body;
+    public Transform bullet;
     private float speed = 210f;
     private static string inputName = "Horizontal";
     private float inputValue;
@@ -14,13 +15,19 @@ public class ActiveTowerRotate : MonoBehaviour {
     }
     // Use this for initialization
 	void Start () {
-        
+
+       
     }
 	
 	// Update is called once per frame
 	void Update () {
 
         inputValue = Input.GetAxis(inputName);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Instantiate(bullet, body.position, body.rotation);
+        }
         
 	}
 
---
+++ Assets/BulletBehavior.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+public class BulletBehavior : MonoBehaviour {
+
+    public Transform bullet;
+    private double angle;
+    private static float speed = 10f;
+    private double x_multiplier;
+    private double z_multiplier;
+    private Vector3 translater;
+
+	// Use this for initialization
+	void Start () {
+
+        x_multiplier = Math.Sin(bullet.eulerAngles.y * Math.PI / 180);
+        z_multiplier = Math.Cos(bullet.eulerAngles.y * Math.PI / 180);
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        translater = 
+            Vector3.forward * (float)(speed * z_multiplier * Time.deltaTime)
+          + Vector3.right * (float)(speed * x_multiplier * Time.deltaTime);
+
+        bullet.Translate(translater);
+
+    }
+}
