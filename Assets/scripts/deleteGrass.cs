0b62e2372c957a64e357d6b979da93d922405287
MattKravitz <m129k419@ku.edu>
2016-11-17 04:00:27 -0600

    grass blocks delete after tower placed

---
+++ Assets/scripts/deleteGrass.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class deleteGrass : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnMouseDown()
+    {
+        Destroy(this.gameObject);
+    }
+}
