3d9fbce4b99fabe2532448f86de70190dbf69a3c
MattKravitz <m129k419@ku.edu>
2016-12-04 21:19:38 -0600

    added functional ui system

--- Assets/scripts/ToggleHandler.cs
+++ Assets/scripts/ToggleHandler.cs
@@ -15,6 +15,18 @@ public class ToggleHandler : MonoBehaviour {
     public Toggle passive2;
     public Toggle passive3;
 
+    public Text tlaser;
+    public Text tlaser1;
+    public Text tlaser2;
+    public Text tlaser3;
+
+    public Text tgunner;
+
+    public Text tpassive;
+    public Text tpassive1;
+    public Text tpassive2;
+    public Text tpassive3;
+
     public static int cost = 100;
 
     private static int thisTower;
@@ -27,17 +39,74 @@ public class ToggleHandler : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         if (laser.isOn) { thisTower = 0; cost = 100; }
-        else if (laser1.isOn) { thisTower = 1; cost = 100; }
-        else if (laser2.isOn) { thisTower = 2; cost = 100; }
-        else if (laser3.isOn) { thisTower = 3; cost = 100; }
+        else if (laser1.isOn) { thisTower = 1; cost = 200; }
+        else if (laser2.isOn) { thisTower = 2; cost = 300; }
+        else if (laser3.isOn) { thisTower = 3; cost = 400; }
 
         else if (gunner.isOn) { thisTower = 4; cost = 50; }
 
-        else if (passive.isOn) { thisTower = 5; cost = 300; }
+        else if (passive.isOn) { thisTower = 5; cost = 200; }
         else if (passive1.isOn) { thisTower = 6; cost = 300; }
-        else if (passive2.isOn) { thisTower = 7; cost = 300; }
-        else if (passive3.isOn) { thisTower = 8; cost = 300; }
-	}
+        else if (passive2.isOn) { thisTower = 7; cost = 400; }
+        else if (passive3.isOn) { thisTower = 8; cost = 500; }
+
+        if(playerWallet.getPlayerMoneyTotal() >= 50)
+        {
+            tgunner.GetComponent <Text>().color = Color.green;
+        }
+        else
+        {
+            tgunner.GetComponent<Text>().color = Color.red;
+        }
+        if (playerWallet.getPlayerMoneyTotal() >= 100)
+        {
+            tlaser.GetComponent<Text>().color = Color.green;
+        }
+        else
+        {
+            tlaser.GetComponent<Text>().color = Color.red;
+        }
+        if (playerWallet.getPlayerMoneyTotal() >= 200)
+        {
+            tlaser1.GetComponent<Text>().color = Color.green;
+            tpassive.GetComponent<Text>().color = Color.green;
+        }
+        else
+        {
+            tlaser1.GetComponent<Text>().color = Color.red;
+            tpassive.GetComponent<Text>().color = Color.red;
+        }
+        if (playerWallet.getPlayerMoneyTotal() >= 300)
+        {
+            tlaser2.GetComponent<Text>().color = Color.green;
+            tpassive1.GetComponent<Text>().color = Color.green;
+        }
+        else
+        {
+            tlaser2.GetComponent<Text>().color = Color.red;
+            tpassive1.GetComponent<Text>().color = Color.red;
+
+        }
+        if (playerWallet.getPlayerMoneyTotal() >= 400)
+        {
+            tlaser3.GetComponent<Text>().color = Color.green;
+            tpassive2.GetComponent<Text>().color = Color.green;
+        }
+        else
+        {
+            tlaser3.GetComponent<Text>().color = Color.red;
+            tpassive2.GetComponent<Text>().color = Color.red;
+        }
+        if (playerWallet.getPlayerMoneyTotal() >= 500)
+        {
+            tpassive3.GetComponent<Text>().color = Color.green;
+        }
+        else
+        {
+            tpassive3.GetComponent<Text>().color = Color.red;
+        }
+
+    }
     
 
     public static int getTower()
