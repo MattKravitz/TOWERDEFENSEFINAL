04b4cc0a7940163086c4804e685f4d904650e2dd
Thomas Doty <Thomas Doty>
2016-12-05 20:33:43 -0600

    Added ending screen

---
+++ Assets/scripts/isGameOver.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class isGameOver : MonoBehaviour {
+
+    private int Health;
+    
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    Health = ActiveTowerRotate.getHealth();
+
+        if(Health < 1)
+        {
+            Application.LoadLevel(2); 
+        }
+    }
+
+    public void checkHealth()
+    {
+    
+    }
+}
