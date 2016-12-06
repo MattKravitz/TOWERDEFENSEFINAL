9bca706f87d33041acbb9c5b98b44e217883f032
Thomas Doty <Thomas Doty>
2016-12-05 19:24:31 -0600

    Added initial start screen

---
+++ Assets/scripts/changeScene.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void changeToGame()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void changeToStart()
+    {
+        Application.LoadLevel(0);
+    }
+}
