3d1862a65b389859aa4dabe2c1e4a6787ea3e081
nBukaty <nickbukaty@gmail.com>
2016-12-06 20:06:42 -0600

    Health tower accesses waveSpawner in a non static way

--- Assets/scripts/healthTower.cs
+++ Assets/scripts/healthTower.cs
@@ -3,25 +3,27 @@ using System.Collections;
 
 public class healthTower : MonoBehaviour {
     public waveSpawner wSpawner;
+    private GameObject thisGame;
     private bool hasHealthIncreased = false;
     public int healthIncrease = 1;
     private int currentHealth;
 	// Use this for initialization
 	void Start () {
         currentHealth = ActiveTowerRotate.getHealth();
-        wSpawner = this.gameObject.GetComponent<waveSpawner>();
+        thisGame = GameObject.Find("GameMaster");
+        wSpawner = thisGame.GetComponentInChildren<waveSpawner>();
     }
 	
 	// Update is called once per frame
 	void Update () {
-        if(waveSpawner.getWaveState() == false && hasHealthIncreased == false){
+        if(wSpawner.getWaveState() == false && hasHealthIncreased == false){
             currentHealth = ActiveTowerRotate.getHealth();
             if (currentHealth < 100)
             {
                 ActiveTowerRotate.setHealth(currentHealth + healthIncrease);
                 hasHealthIncreased = true;
             }
-        } else if(waveSpawner.getWaveState() == true && hasHealthIncreased == true)
+        } else if(wSpawner.getWaveState() == true && hasHealthIncreased == true)
         {
             hasHealthIncreased = false;
         }
