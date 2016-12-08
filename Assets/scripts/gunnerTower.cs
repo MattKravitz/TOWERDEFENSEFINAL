4841d5afb0656204dbbc6d8e764182280a0229b9
David Cuellar <dcuellar@ku.edu>
2016-12-08 13:27:27 -0600

    gunnerTowerUpdate

---
+++ Assets/scripts/gunnerTower.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gunnerTower : towerBaseClass {
+
+    [Header("Tower Attack Attributes")]
+    public float towerShotSpeed = 2f;
+    //public int towerDamage = 1;
+ 
+
+    [Header("Projectile Variables")]
+    public GameObject projectile;
+    public float shotCooldown = 1f;
+
+	// Use this for initialization
+	void Start ()
+    {
+        InvokeRepeating("refreshTarget", 2f, .5f);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (m_target == null)
+        {
+            //return; //empty return statement to kill update function if no target exists.
+        }
+        else
+        {
+            acquireTarget();
+            if (shotCooldown <= 0f)
+            {
+                fire();
+                shotCooldown = 1f / towerShotSpeed;
+            }
+
+            shotCooldown -= Time.deltaTime;
+        }
+    }
+    virtual public void fire()
+    {
+        /**This is a temporary position configuration for the prototype only
+        *   this will produce awkward graphics since the projectile will be launched from the same coordinate as the tower.
+        **/
+
+        GameObject projectileShot = (GameObject)Instantiate(projectile, m_towerPosition.position, m_towerPosition.rotation);
+        projectile projectile1 = projectileShot.GetComponent<projectile>();
+
+        projectile1.setDamage(towerDamage);
+        //projectile1.setShotPoint(shootPosition.position);
+
+        if (projectile != null)
+        {
+            
+            projectile1.trace(m_target.transform, targetedEnemy);
+        }
+
+    }
+
+    public void setTowerShotSpeed(float shotSpeed)
+    {
+        towerShotSpeed = shotSpeed;
+
+    }
+
+    public float getTowerSpeed()
+    {
+        return towerShotSpeed;
+    }
+
+   // public void setTowerDamage(int damage)
+  //  {
+       // towerDamage = damage;
+
+   // }
+
+   // public int getTowerDamage()
+   // {
+      //  return towerDamage;
+   // }
+}
---
+++ Assets/scripts/towerBaseClass.cs
@@ -0,0 +1,108 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class towerBaseClass : MonoBehaviour {
+
+    [Header("Tower Attributes")]
+    public float towerRange = 3f;
+    public Transform m_target;
+    public enemy targetedEnemy;
+    public string towersTag = "placed_tower";
+    public int towerDamage = 1;
+    //public Collider towerCollider;
+
+    [Header("Targeting Variables")]
+    public string enemiesTag = "enemy";
+    public Transform m_towerPosition;
+    public Transform m_rotatingPiece;
+    public float rotationVelocity = 5f;
+
+
+    [Header("Economy Interfacing Variables")]
+    public int towerCost;
+    public int towerValue;
+
+    public  void acquireTarget()
+    {
+        Vector3 direction = m_target.position - m_towerPosition.position;
+        Quaternion towerRotation = Quaternion.LookRotation(direction);
+        Vector3 convertedRotation = Quaternion.Lerp(m_rotatingPiece.rotation, towerRotation, Time.deltaTime * rotationVelocity).eulerAngles;
+        m_rotatingPiece.rotation = Quaternion.Euler(0f, convertedRotation.y, 0f);
+    }
+
+    virtual public void refreshTarget()
+    {
+        GameObject[] enemiesArray = GameObject.FindGameObjectsWithTag(enemiesTag);
+        float smallestDistance = Mathf.Infinity;
+        GameObject closestEnemy = null;
+
+        foreach (GameObject enemy in enemiesArray)
+        {
+            float enemyDistance = Vector3.Distance(transform.position, enemy.transform.position);
+
+            if (enemyDistance <= smallestDistance)
+            {
+                smallestDistance = enemyDistance;
+                closestEnemy = enemy;
+                
+            }
+
+        }
+
+        if (closestEnemy != null && smallestDistance <= towerRange)
+        {
+            
+            m_target = closestEnemy.transform;
+            targetedEnemy = closestEnemy.GetComponent<enemy>();
+            //targetedEnemy.setHealth(5);
+        }
+        else
+        {
+            m_target = null;
+            //Debug.Log("No Target");
+        }
+    }
+
+    public  void setTowerRange(float range)
+    {
+        towerRange = range;
+    }
+    public  float getTowerRange()
+    {
+        return towerRange;
+    }
+    public  void setTowerTarget(Transform target)
+    {
+        m_target = target;
+    }
+    public  Transform getTowerTarget()
+    {
+        return m_target;
+    }
+    public void setTargetedEnemy(enemy newEnemy)
+    {
+        targetedEnemy = newEnemy;
+    }
+    public enemy getTargetedEnemy()
+    {
+        return targetedEnemy;
+    }
+    public void setTowerCost(int cost)
+    {
+        towerCost = cost;
+    }
+    public int getTowerCost()
+    {
+        return towerCost;
+    }
+   
+    public int getTowerDamage()
+    {
+        return towerDamage;
+    }
+
+    public void setTowerDamage(int dmg)
+    {
+        towerDamage = dmg;
+    }
+}
