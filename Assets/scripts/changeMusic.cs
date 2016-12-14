a3508bb7260499ee088963b3fe8cab93875ec19f
MattKravitz <m129k419@ku.edu>
2016-12-13 19:08:23 -0600

    bunch of sound effects, none final tho

---
+++ Assets/scripts/changeMusic.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class changeMusic : MonoBehaviour {
+
+    public AudioClip slow;
+    public AudioClip fast;
+    private AudioSource sound;
+    private int a = 0;
+
+	// Use this for initialization
+	void Start () {
+        sound = gameObject.GetComponent<AudioSource>();
+        sound.clip = slow;
+        sound.Play();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+        if(GameObject.FindGameObjectWithTag("enemy") == null && a ==0)
+        {
+            a = 1;
+            sound.Stop();
+            sound.clip = slow;
+            sound.Play();
+        }
+        else if (GameObject.FindGameObjectWithTag("enemy") != null && a == 1)
+        {
+            a = 0;
+            sound.Stop();
+            sound.clip = fast;
+            sound.Play();
+            
+        }
+    
+    }
+}
